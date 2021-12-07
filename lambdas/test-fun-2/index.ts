55351a269f9183b0fcdc103252091af1c71e34a7
Tanishq Singh <>
2021-12-07 14:50:10 +0530

    hello

--- lambdas/test-fun-2/index.ts
+++ lambdas/test-fun-2/index.ts
@@ -4,6 +4,11 @@ import * as _ from 'lodash';
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
     return {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body: JSON.stringify({
             event,
             smt: `The random value (max 999) is: ${_.random(999)}`
