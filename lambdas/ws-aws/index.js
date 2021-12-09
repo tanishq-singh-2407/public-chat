416f127d9dd8fe9433d633219fc66ec33e92e19b
Tanishq Singh <>
2021-12-09 23:07:42 +0530

    ws-aws

---
+++ lambdas/ws-aws/index.js
@@ -0,0 +1,47 @@
+const { ApiGatewayManagementApi, DynamoDB } = require('aws-sdk');
+
+const api = new ApiGatewayManagementApi({ endpoint: 'https://90v2ormm5c.execute-api.ap-south-1.amazonaws.com/test', region: 'ap-south-1' });
+const dynamo = new DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: 'ap-south-1' });
+
+const sendMessage = async (ConnectionId, Data) => await api.postToConnection({ ConnectionId, Data }).promise();
+
+exports.handler = async event => {
+    const id = event.requestContext.connectionId;
+
+    try {
+        switch (event.requestContext.routeKey) {
+            case "$connect":
+                await dynamo.put({ TableName: 'ws-aws', Item: { id } }).promise();
+                break;
+
+            case "$disconnect":
+                await dynamo.delete({ TableName: 'ws-aws', Key: { id } }).promise();
+                break;
+
+            case "publicMessage":
+                const connectionData = await dynamo.scan({ TableName: "ws-aws" }).promise();
+                await Promise.all(connectionData.Items.map(async ({ id }) => await sendMessage(id, JSON.parse(event.body).message)));
+                break;
+
+            default:
+                break;
+        };
+    } catch (err) {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                fromServer: true,
+                error: err
+            })
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            fromServer: true,
+            error: false,
+            connectionId: event.requestContext.connectionId
+        })
+    };
+};
